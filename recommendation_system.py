a2f020ebbfc9337deb4d6b42c540249fb11becff
dsherbini <111007109+dsherbini@users.noreply.github.com>
2024-03-12 13:46:55 -0500

    Update recommendation_system.py

--- recommendation_system.py
+++ recommendation_system.py
@@ -4,14 +4,13 @@ Title: K-Drama Recommendation System
 Date: March 2023
 """
 
-# basic packages
+# load packages
 import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
-# machine learning packages
-
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics import jaccard_score
 
 # set wd
 PATH = '/Users/danya/Documents/GitHub/personal github/kdrama-recommendations'
@@ -19,49 +18,60 @@ PATH = '/Users/danya/Documents/GitHub/personal github/kdrama-recommendations'
 # import k-drama data
 kdramas = pd.read_csv(os.path.join(PATH, 'kdramas_features'))
 
+################################ DATA CLEANING ################################
 
-
-import numpy as np
-from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.metrics import jaccard_score
-
-
+# drop review columns from the df
 kdramas = kdramas.drop(['Review','Reviews_Clean'],axis = 1)
-cont = kdramas.iloc[:,1:19]
-bina = kdramas.iloc[:,20:]
 
-def recommend_kdrama(selected_item, item_features, n_recommendations=5):
+# fill NaNs with the general polarity scores for all continuous feature columns
+kdramas = kdramas.apply(lambda row: row.fillna(row['Polarity_Score']), axis=1)
+
+# split into continuous and binary features
+continuous_features = kdramas.iloc[:,1:19]
+binary_features = kdramas.iloc[:,20:]
+
+
+############################### RECOMMENDATIONS ###############################
+
+
+def recommend_kdrama(selected_title, continuous_features, binary_features, n_recommendations=5):
     '''
     Recommend kdramas to a user based on the selection of kdrama they have liked/watched. 
     Measures similarity between kdramas using cosine similarity for continuous features
     and Jaccard similarity for binary features.
     
     Parameters:
-    selected_item: Title of the k-drama selected by the user
-    item_features: A matrix where each row represents an kdrama (item) and each column represents a feature.
-    n_recommendations: The number of recommendations to make.
+    selected_title: Title of the k-drama selected by the user
+    continuous_features: a df/matrix of continuous features where each column represents a kdrama
+    binary_features: a df/matrix of binary features where each column represents a kdrama
+    n_recommendations: The number of recommendations to make (default is 5)
     
     Returns:
     --------
     A list of recommended k-drama titles
     '''
-    # separate features into binary and continuous
-    continuous = ['']
-    continuous_features = item_features.iloc[:,1:19]
-    binary_features = item_features[:, num_continuous_features:]
     
-    # Compute cosine similarity for continuous features
+    # compute cosine similarity for continuous features
     cosine_sim = cosine_similarity(continuous_features)
     
-    # Compute Jaccard similarity for binary features
-    # Note: This is a simplified example. You might need to adapt this for your specific binary data structure.
+    # compute Jaccard similarity for binary features
     jaccard_sim = jaccard_score(binary_features, binary_features, average=None)
     
-    # Combine the two similarity measures (e.g., by averaging)
+    # take the average of the two similarity scores
     combined_sim = (cosine_sim + jaccard_sim) / 2
     
-    # Get the top N most similar items
-    top_similar_items = np.argsort(combined_sim[user_id], axis=0)[-num_recommendations:]
+    # get the top N most similar items
+    top_similar_items = np.argsort(combined_sim[selected_title], axis=0)[-n_recommendations:]
     
     return top_similar_items
 
+cosine_sim = cosine_similarity(continuous_features)
+    
+    # compute Jaccard similarity for binary features
+jaccard_sim = jaccard_score(binary_features, binary_features, average=None)
+    
+    # take the average of the two similarity scores
+combined_sim = (cosine_sim + jaccard_sim) / 2
+    
+    # get the top N most similar items
+top_similar_items = np.argsort(combined_sim[selected_title], axis=0)[-n_recommendations:]
