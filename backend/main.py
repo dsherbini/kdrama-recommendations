057b66a2737821fe1c7d85fa4f18ca10aef221ff
Danya Sherbini <danyasherbini9@gmail.com>
2025-08-28 14:29:55 -0500

    update main.py cors url

--- backend/main.py
+++ backend/main.py
@@ -17,14 +17,14 @@ app = FastAPI()
 # List all allowed origins
 #origins = [
 #    "http://localhost:3000",  # Local dev
-#    "https://kdramarama-staging-d400cc47efde.herokuapp.com/",  # Staging frontend
+#    "https://kdramarama-staging-d400cc47efde.herokuapp.com",  # Staging frontend
 #    "https://kdramarama-production-169470a72f48.herokuapp.com",  # Production frontend
 #]
 
 # Enable CORS for local dev and Heroku frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://kdramarama-staging-d400cc47efde.herokuapp.com/"],
+    allow_origins=["https://kdramarama-staging-d400cc47efde.herokuapp.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
